5077efb58b9f2b0bfa02c5ae6d9543ed4638e8fe
Dibertz Soft <diegodibertz@gmail.com>
2022-02-22 17:53:18 -0300

    File SampleScen.cs <EditorDescription> updated

--- rts_camera_move/SampleScene.cs
+++ rts_camera_move/SampleScene.cs
@@ -12,7 +12,7 @@ public class SampleScene : Spatial
         sampleScene = (PackedScene)ResourceLoader.Load("res://addons/rts_camera_move/SampleScene.tscn");
         var RTS_Scene = sampleScene.Instance();
         AddChild(RTS_Scene);
-        this.EditorDescription= ("Northgard RTS Camera\nInstructions for Use - Input Map\n\n1.Configure the camera zoom:\n\n• Wheel Up Button is 'ui_scroll_down'\n• Wheel Down Button is 'ui_scroll_up'\n\n2.Configure the character movement in the Input Map:\n\n• Physical Key A is 'ui_left'\n• Physical Key D is 'ui_right'\n• Physical Key W is 'ui_up'\n• Physical Key S is 'ui_down'\n\n~Enjoy it!.");
+        this.EditorDescription= ("Northgard RTS Camera\nInstructions for Use - Input Map\n\n1. Create the camera zoom input map:\n\n• Wheel Up Button is 'ui_scroll_down'\n• Wheel Down Button is 'ui_scroll_up'\n\n2. Add the character move input map:\n\n• Physical Key A is 'ui_left'\n• Physical Key D is 'ui_right'\n• Physical Key W is 'ui_up'\n• Physical Key S is 'ui_down'\n\n3. Create the lock camera input map:\n\n• Physical Key Y is 'ui_camera'\n\n~Enjoy it!.");
     }
 
     public void load_rts_scene()
